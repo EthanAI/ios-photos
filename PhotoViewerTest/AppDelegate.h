aab13077518220c882a733c72aa073525b637279
ethanai <ethan@selfawarelab.com>
2014-09-23 16:47:44 -1000

    Initial Commit

---
+++ PhotoViewerTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoViewerTest
+//
+//  Created by Ethan Smith on 9/23/14.
+//  Copyright (c) 2014 Ethan Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoViewerTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoViewerTest
+//
+//  Created by Ethan Smith on 9/23/14.
+//  Copyright (c) 2014 Ethan Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
